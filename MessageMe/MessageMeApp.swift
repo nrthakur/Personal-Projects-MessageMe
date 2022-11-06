5d1c812858bda98f8ec68f8fc21836fb13dfc267
nrthakur <115727511+nrthakur@users.noreply.github.com>
2022-11-06 10:07:59 -0500

    Initial Commit

---
+++ MessageMe/MessageMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMeApp.swift
+//  MessageMe
+//
+//  Created by Nikunj Thakur on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct MessageMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
